a1c79ee66643b91be6926ce041e9c0403cd126c6
Грибок Арсентий <Gribok.work@gmail.com>
2019-02-16 16:20:32 +0200

    Update mongoKeys.js
    Ключ все равно засветился в коммитах...

--- server/config/mongoKeys.js
+++ server/config/mongoKeys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    devKey: 'mongodb+srv://card_db_dev_user:<PASSWORD>@cardsapp-svwye.gcp.mongodb.net/test?retryWrites=true"'
+    devKey: 'mongodb+srv://card_db_dev_user:miPafrJ7Veg24tb@cardsapp-svwye.gcp.mongodb.net/test?retryWrites=true"'
 };
